165f94dc19748ca63a2f7ebb10c3fa74b4b86646
Харм Алексей <a.harm@systtech.ru>
2021-04-11 19:35:23 +0200

    added dllexport

--- value.hpp
+++ value.hpp
@@ -1,3 +1,19 @@
 #pragma once
 
-extern "C" int value();
+#ifdef _WIN32
+#  if defined(value_shared_EXPORTS)
+#    define VALUE_EXPORT __declspec(dllexport)
+#  elif defined(value_shared_IMPORTS)
+#    define VALUE_EXPORT __declspec(dllimport)
+#  else
+#    define VALUE_EXPORT
+#  endif
+#else
+#  define VALUE_EXPORT
+#endif
+
+extern "C" {
+
+VALUE_EXPORT int value();
+
+}
