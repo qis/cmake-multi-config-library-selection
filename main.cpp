9f1b2e0048031d316b454d0f1612c5a0642703c0
Харм Алексей <a.harm@systtech.ru>
2021-04-11 19:19:38 +0200

    removed extra stuff

--- main.cpp
+++ main.cpp
@@ -1,22 +1,7 @@
 #include "value.hpp"
 #include <string>
 #include <cstdio>
-#include <cerrno>
-#include <dlfcn.h>
-
-struct Display;
-Display* XOpenDisplay(const char* name);
 
 int main() {
   std::puts(std::to_string(value()).data());
-  const auto handle = dlopen("/usr/lib/x86_64-linux-gnu/libX11.so", RTLD_NOW);
-  if (!handle) {
-    std::fprintf(stderr, "could not open libX11.so: %s\n", dlerror());
-    return 1;
-  }
-  const auto XOpenDisplayProc = reinterpret_cast<decltype(&XOpenDisplay)>(dlsym(handle, "XOpenDisplay"));
-  if (!XOpenDisplayProc) {
-    std::fputs("could not get function address: XOpenDisplay\n", stderr);
-    return 1;
-  }
 }
